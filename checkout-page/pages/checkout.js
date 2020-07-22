716edeb926d03c17aba8c53afdf61c552f9ef4af
Henrique Weiand <henriqueweiand@gmail.com>
2020-07-21 21:16:00 -0300

    update

--- checkout-page/pages/checkout.js
+++
@@ -1,7 +0,0 @@
-import { CheckoutPage } from 'micro-frontend-react-pages';
-
-import 'micro-frontend-react-pages/src/scss/pages/checkout.scss';
-
-const Checkout = () => <CheckoutPage />
-
-export default Checkout;
--- docs-page/pages/docs.js
+++
@@ -1,7 +0,0 @@
-import { DocsPage } from 'micro-frontend-react-pages';
-
-import 'micro-frontend-react-pages/src/scss/pages/home.scss';
-
-const Docs = () => <DocsPage />
-
-export default Docs;
